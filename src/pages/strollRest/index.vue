f2675c00e4cee1988919b92a3bae764f2b8f301f
lu-jing <641442798@qq.com>
2020-07-02 21:42:28 +0800

    游憩类

--- src/pages/strollRest/index.vue
+++ src/pages/strollRest/index.vue
@@ -21,6 +21,31 @@
       <div class="type-item"  @click="typeIn=0" :class="typeIn==0 ? 'active':''">游憩类建筑</div>
       <div class="type-item" @click="typeIn=1" :class="typeIn==1 ? 'active':''">服务类建筑</div>
     </div>
+    <div class="swiper area-swiper">
+      <swiper
+        class="swiper-inner"
+        :indicator-dots='false'
+        :autoplay='false'
+        :interval='3000'
+        :circular='circular'
+        :previous-margin="'100px'"
+        :next-margin="'150px'"
+        @change='changeSwiper'
+        v-if="showSwiper"
+      >
+        <a  v-for='img in topSwipers' :key='img.id'>
+
+          <swiper-item class="swiper-bar" :item-id='img.id'>
+            <img              
+              class='slide-image' 
+              mode='aspectFit' 
+              :src="img.outterImage" 
+              />
+          </swiper-item>
+        </a>
+      </swiper>  
+      
+    </div>
     <div class="gl-body-head">
       <div class="three-switch">
         <div class="switch-item" @click="index=0" :class="index==0 ? 'current':''">off</div>
@@ -32,37 +57,39 @@
       <div class="bet-item">
         <div class="bet-item-title">
           <div class="bet-item-left">
-            <img class="greenarea" src="/static/icon/greenarea.png" alt="">绿化用地面积
+            <img class="greenarea" src="/static/icon/play.png" alt="">活动馆用地
           </div>
-          <div class="bet-item-right">205860㎡</div>
+          <div class="bet-item-right">2个</div>
         </div>
+        <div class="bet-item-be">500㎡</div>
         <div class="green-pro-one">
-          <progress class="greenpro" percent="79" color="#5380FF" border-radius="5" stroke-width="3"></progress>
+          <progress class="greenpro" percent="79" color="#5380FF" border-radius="5" stroke-width="4"></progress>
           <div class="green-pro-left">79%</div>
         </div>
       </div>
       <div class="bet-item">
         <div class="bet-item-title">
           <div class="bet-item-left">
-          <img class="greenarea" src="/static/icon/avegreen.png" alt="">人均绿地面积
+            <div class="number">1</div>活动馆1
           </div>
         </div>
         <div class="green-pro">
-          <slider class="greenslider" backgroundColor="#EBEBEB" activeColor="#5380FF" value="69"/>
-          <div class="green-pro-left">20㎡/人</div>
+          <van-slider class="greenslider" bar-height="3.1px" active-color="#5380FF" inactive-color="#EBEBEB" value="50" @change="onChange" />
+          <div class="green-pro-left">15㎡/人</div>
         </div>
       </div>
       <div class="bet-item">
         <div class="bet-item-title">
           <div class="bet-item-left">
-            <img class="greenarea" src="/static/icon/avewater.png" alt="">人均水域面积
+            <div class="number">2</div>活动馆2
           </div>
         </div>
         <div class="green-pro">
-          <slider class="greenslider" backgroundColor="#EBEBEB" activeColor="#5380FF" value="69"/>
-          <div class="green-pro-left">180㎡/人</div>
+          <van-slider class="greenslider" bar-height="3.1px" active-color="#5380FF" inactive-color="#EBEBEB" value="50" @change="onChange" />
+          <div class="green-pro-left">20㎡/人</div>
         </div>
       </div>
+      <div class="add"><div class="number">+</div>添加类目</div>
     </div>
     <div class="submit">提交修改</div>
   </div>
@@ -119,10 +146,27 @@ export default {
       echarts,
       onInit: initChart,
       index: 0,
-      typeIn: 0
+      typeIn: 0,
+      showSwiper: true,
+      topSwipers: [{
+        id: 'huodong',
+        outterImage: '../../static/images/dlrb.jpg'
+      },
+      {
+        id: 'zhanlan',
+        outterImage: '../../static/images/dlrb.jpg'
+      },
+      {
+        id: 'tinglang',
+        outterImage: '../../static/images/dlrb.jpg'
+      }]
     }
   },
   methods: {
+    changeSwiper (event) {
+      console.log(event);
+      
+    }
     // sliderchange() {
     //   console.log(2333)
     // }
@@ -239,6 +283,15 @@ export default {
   justify-content: space-between;
   align-items: center;
 }
+.bet-item-be{
+  font-size:15px;
+  font-weight:600;
+  color:#333;
+  margin: 5px 0px 0px 33px;
+}
+.bet-item-left{
+  display: flex;
+}
 .green-pro{
   margin-top:10px;
   display: flex;
@@ -250,7 +303,6 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 5px 18px;
 }
 .green-pro-left{
   font-size:15px;
@@ -267,10 +319,30 @@ export default {
 .greenarea{
   width: 22px;
   height: 22px;
-  background: #0FC0A3;
+  background: #5480FF;
   border-radius: 11px;
   vertical-align: bottom;
   margin-right: 11px;
+}
+.add{
+  font-size:15px;
+  font-weight:600;
+  color:#333;
+  margin: 0px 0px 38px;
+  display: flex;
+}
+.number{
+  width: 22px;
+  height: 22px;
+  background: #5480FF;
+  border-radius: 11px;
+  vertical-align: bottom;
+  margin-right: 11px;
+  color: #ffffff;
+  font-size:15px;
+  font-weight:400;
+  line-height:21px;
+  text-align: center;
 }
 .submit{
   width: 315px;
@@ -302,5 +374,20 @@ export default {
 .echarts-wrap {
   width: 50%;
   height: 150px;
+}
+.area-swiper {
+  height: 60px;
+  margin-top: 20px;
+}
+.swiper-inner {
+  height: 100%;
+}
+.swiper-bar {
+  text-align: center;
+}
+.slide-image{
+  width:134px;
+  height:60px;
+  margin: 0 10px;
 }
 </style>
